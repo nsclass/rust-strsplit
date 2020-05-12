4dceb6b755326e273bb2bfff09df777adc1f10d3
Nam Seob Seo <nseo@pros.com>
2020-05-12 10:54:06 -0500

    rust strsplit practice

---
+++ src/lib.rs
@@ -0,0 +1,78 @@
+//!
+#![warn(missing_debug_implementations, rust_2018_idioms, missing_docs)]
+
+
+pub struct StrSplit<'haystack, D> {
+  remainder: Option<&'haystack str>,
+  delimiter: D,  
+}
+
+
+pub trait Delimiter {
+  fn find_next(&self, s: &str) -> Option<(usize, usize)>;
+}
+
+impl<'haystack, D> StrSplit<'haystack, D> {
+  pub fn new(haystack: &'haystack str, delimiter: D) -> Self {
+    Self {
+      remainder: Some(haystack),
+      delimiter,
+    }
+  }
+}
+
+// let x: StrSplit;
+// for part in x {
+// 
+// }
+impl<'haystack, D> Iterator for StrSplit<'haystack, D> 
+where D: Delimiter {
+  type Item = &'haystack str;
+
+  fn next(&mut self) -> Option<Self::Item> {
+    let remainder = self.remainder.as_mut()?;
+    if let Some((delim_start, delim_end)) = self.delimiter.find_next(remainder) {
+      let until_delimiter = &remainder[..delim_start];
+      (*remainder) = &remainder[delim_end..];
+      Some(until_delimiter)
+    } else {
+      self.remainder.take()
+    }
+  }
+}
+
+impl Delimiter for &str {
+  fn find_next(&self, s: &str) -> Option<(usize, usize)> {
+    s.find(self).map(|start| (start, start + self.len())) 
+  }
+}
+
+impl Delimiter for char {
+  fn find_next(&self, s: &str) -> Option<(usize, usize)> {
+    s.char_indices().find(|(_, c)| c == self).map(|(start, _)| (start, start + 1))
+  }
+}
+
+pub fn until_char(s: &str, c: char) -> &str {
+  StrSplit::new(s, c).next().expect("StrSplit always gives at least on result")
+}
+
+#[test]
+fn until_char_test() {
+  assert_eq!(until_char("hello world", 'o'), "hell")
+}
+
+#[test]
+fn it_works() {
+  let haystack = "a b c d e";
+  let letters: Vec<_> =  StrSplit::new(haystack, " ").collect();
+  assert_eq!(letters, vec!["a", "b", "c", "d", "e"]);
+}
+
+
+#[test]
+fn tail() {
+  let haystack = "a b c d ";
+  let letters: Vec<_> =  StrSplit::new(haystack, " ").collect();
+  assert_eq!(letters, vec!["a", "b", "c", "d", ""]);
+}
